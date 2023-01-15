342f86e715c1995fda75ae1468d69bb2cb8c8d59
Alexandra <alexandra@192.168.1.38>
2023-01-15 19:29:01 +0300

    init: start reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n >= 0) {
+        return Math.abs(String(+n).split('').reverse().join(''))
+    }else{
+        return Math.abs(String(-n).split('').reverse().join(''))
+    }
+};
